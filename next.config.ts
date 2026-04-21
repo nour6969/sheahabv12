b4930a86b453ed4ab0e470ddd50f26b9ea6f7b7f
nour6969 <pubgnour132@gmail.com>
2026-04-21 21:02:32 +0200

    Change image hostname from picsum to postimg

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**', // This allows any path under the hostname
       },
